71d72d67c73d92b2b55519b67d6b7e7612ff03f6
LucCook <luc_cook@hotmail.co.uk>
2023-01-23 15:08:48 +0000

    starts login & register page

--- fe-socialift/App.js
+++ fe-socialift/App.js
@@ -1,23 +1,18 @@
-import * as React from 'react';
-import {NavigationContainer} from '@react-navigation/native';
-import {LoginPage} from './components/LoginPage'
+import * as React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { LoginPage } from "./components/LoginPage/LoginPage";
 
 const App = () => {
-  const [user, setUser] = React.useState('')
+  const Stack = createNativeStackNavigator();
+  const [user, setUser] = React.useState("");
   return (
-
     <NavigationContainer>
-      {/* Rest of your app code */}
-      {/* {!user && <LoginPage/>} */}
-      <LoginPage/>
-
-
-
+      <Stack.Navigator screenOptions={{headerShown: false}}>
+        <Stack.Screen name="Login" component={LoginPage} />
+      </Stack.Navigator>
     </NavigationContainer>
   );
 };
-
-
-
 
 export default App;
---
+++ fe-socialift/components/LoginPage/LoginPage.jsx
@@ -0,0 +1,30 @@
+import {
+  StyleSheet,
+  View,
+  SafeAreaView,
+  Text,
+  Alert,
+  TextInput,
+} from "react-native";
+import React, { useState } from "react";
+import { styles } from "./LoginPageStyle.js";
+import { TextField, Button } from "@mui/material";
+
+export const LoginPage = () => {
+  const [showLogin, setShowLogin] = useState(false)
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const CheckLoginDetails = () => {
+    // checks username and password with backend
+  };
+
+  return (
+    <View style={styles.view}>
+      {showLogin && <TextField style={styles.input} variant="outlined" placeholder="Username" />}
+      {showLogin && <TextField style={styles.input} type="password" placeholder="Password" />}
+      <Button style={styles.loginButton} variant="contained" onClick={() => {setShowLogin(true)}}>Login</Button>
+      
+    </View>
+  );
+};
