f73f535fc9c99b58f2e48b386c8a8e2ea2724cca
tomaszkrupa1 <tomaszkrupa1999@gmail.com>
2023-01-25 17:07:18 +0000

    edit styling

---
+++ fe-socialift/components/HomePage/HomePage.jsx
@@ -0,0 +1,19 @@
+import {
+    StyleSheet,
+    View,
+    SafeAreaView,
+    Text,
+    Alert,
+    TextInput,
+  } from "react-native";
+
+
+  export const HomePage = ({Navigation}) => {
+
+      console.log("THIS IS IN THE CONSOLE")
+return ( 
+ <Text>THIS IS THE HOME PAGE</Text>
+)
+
+
+  }
