9f1bfb16ac644f7901c827a7bbdafbb32081fa6b
LucCook <luc_cook@hotmail.co.uk>
2023-02-01 20:20:03 +0000

    removes auto capitalising fields on login page

--- fe-socialift/components/LoginPage/LoginPage.jsx
+++ fe-socialift/components/LoginPage/LoginPage.jsx
@@ -208,6 +208,7 @@ export const LoginPage = ({ navigation }) => {
                   : " "
               }
               autoCorrect={false}
+              autoCapitalize={false}
               ref={usernameRef}
             />
           )}
@@ -221,6 +222,7 @@ export const LoginPage = ({ navigation }) => {
               }}
               errorMessage={emailErrorMessage}
               autoCorrect={false}
+              autoCapitalize={false}
               ref={emailRef}
             />
           )}
@@ -236,6 +238,7 @@ export const LoginPage = ({ navigation }) => {
                 passwordError ? "Password must be at least 8 characters" : " "
               }
               autoCorrect={false}
+              autoCapitalize={false}
               secureTextEntry={true}
               ref={passRef}
             />
@@ -251,6 +254,7 @@ export const LoginPage = ({ navigation }) => {
               errorMessage={confirmError ? "Passwords must match" : " "}
               autoCorrect={false}
               secureTextEntry={true}
+              autoCapitalize={false}
               ref={confirmRef}
             />
           )}
