159a0c55ba13bccb2ba03a4fe51ccf62b12c59ac
kawooky <kawooky@gmail.com>
2023-02-01 20:38:06 +0000

    groups for luc

--- fe-socialift/components/GroupPage/GroupPage.jsx
+++ fe-socialift/components/GroupPage/GroupPage.jsx
@@ -6,9 +6,13 @@ import {
     Alert,
     TextInput,
     Image,
-    Picker
+    Picker,
+    Dimensions
   } from "react-native";
+  import { MaterialCommunityIcons } from '@expo/vector-icons';
+  import {BarChart, PieChart, StackedBarChart} from "react-native-chart-kit";
   import { styles } from "./GroupPageStyle.js";
+  import NavBar from "../NavBar/NavBar";
   import { Avatar, Button } from '@rneui/themed';
   import React, { useEffect, useState } from 'react';
   import { getFirebase } from "../../firebase.js";
@@ -16,22 +20,25 @@ import {collection, getFirestore, getDocs, getDoc, addDoc, doc} from "firebase/f
 import { Table, TableWrapper, Row, Rows, Col, Cols, Cell } from 'react-native-table-component';
 
 
-// const { auth } = getFirebase();
-// const loggedInUser = auth.currentUser
-// const loggedInUserName = loggedInUser.displayName
-// console.log(loggedInUser, '<<< logged in user')
+
 
 
 export const GroupPage = ({ route, navigation}) => {
+  const {groupId} = route.params
+  console.log(groupId, '<groupid')
+
     const [exercise, setExercise] = useState('');
     const [graphOrTable, setGraphOrTable] = useState('table')
     const [groupObj, setGroupObj] = useState({})
     const [members, setMembers] = useState([])
     const db = getFirestore();
 
-    const {groupId} = route.params
-    console.log(groupId, '<<< params')
-  // console.log(groupId)
+    const { auth } = getFirebase();
+    const loggedInUser = auth.currentUser
+    const loggedInUserName = loggedInUser.displayName
+    console.log(loggedInUser, '<<< logged in user')
+
+
     useEffect(()=>{
       getDoc(doc(db, 'groups', groupId))
       .then((group)=>{
@@ -45,22 +52,35 @@ export const GroupPage = ({ route, navigation}) => {
         }))
       })
 
+      const { auth } = getFirebase();
+const loggedInUser = auth.currentUser
+const loggedInUserName = loggedInUser.displayName
+console.log(loggedInUser, '<<< logged in user')
+
     }, [])
 
-
+    //for the table
     const tableHead = ['Members', 'Squat', 'Deadlift', 'Bench Press', 'Total'];
     const tableTitle = members.map((member)=>{
       return member.username})
-    const tableDta = [
-      ['qw', 'we', 'r'],
-      ['qw', 'wq', 'w'],
-    ];
+
     const tableData = members.map((member)=>{
-      return [member.SquatMax, member.ChestMax, member.DeadliftMax, member.SquatMax+ member.ChestMax+ member.DeadliftMax ]
+      return [member.squatMax, member.chestMax, member.deadliftMax, member.squatMax+ member.chestMax+ member.deadliftMax ]
     })
 
 
-  
+    //for the bar chart
+    // const chartLabels= [...tableTitle]
+    const legend = ['Squat', 'Deadlift', 'Bench Press']
+    const chartData = members.map((member)=>{
+      return [ member.squatMax, member.chestMax, member.deadliftMax ]})
+
+    //   console.log(chartLabels, '<<<chart labels')
+    // console.log(chartData, '<<<chart data')
+
+    const chartLabels = members.map((member)=>{
+      return member.username})
+
 
 
 
@@ -84,7 +104,7 @@ export const GroupPage = ({ route, navigation}) => {
 
           <Button
           onPress={()=>{
-            navigation.navigate('GroupMessaging', {groupId, groupName: groupObj.group_name})
+            navigation.navigate('GroupMessaging', {groupId, groupName: groupObj.group_name , loggedInUserName})
           }}
           title='Message'/>
         <View style={styles.membersContainer}>
@@ -97,13 +117,16 @@ export const GroupPage = ({ route, navigation}) => {
                     sx={{ width: 35, height: 35 }}
                   />)
                 })}
-        <Button >Add to Group</Button>
+          
+        <Button >Edit Group</Button>
         </View>
 
         { graphOrTable === 'graph' && (
           <Button 
           onPress={()=>{
             setGraphOrTable('table')
+            console.log(chartLabels, '<<<chart labels')
+            console.log(chartData, '<<<chart data')
           }}
         title='Switch to table'/>
         )}
@@ -112,6 +135,8 @@ export const GroupPage = ({ route, navigation}) => {
           onPress={()=>{
             setGraphOrTable('graph')
             console.log(loggedInUser, '<<<logged in user')
+            console.log(loggedInUserName, '<<<name')
+
           }}
         title='Switch to graph'/>
         )}
@@ -127,6 +152,48 @@ export const GroupPage = ({ route, navigation}) => {
         </Table>
       </View> 
 
+
+
+
+
+      {/* <StackedBarChart
+  data={chartData}
+  width={340}
+  height={220}
+  strokeWidth={16}
+  radius={20}
+  style={{
+    marginVertical: 8,
+    borderRadius: 16
+  }}
+  hideLegend={false}
+/> */}
+{/* <StackedBarChart
+    data={{
+        labels: chartLabels,
+        legend: legend,
+        data: {chartData},
+        barColors: ["#dfe4ea", "#ced6e0", "#a4b0be"]
+    }}
+    width={Dimensions.get("window").width - 50} // from react-native
+    height={220}
+    yAxisLabel={"Rp"}
+    chartConfig={{
+    backgroundColor: "blue",
+    backgroundGradientFrom: "blue",
+    backgroundGradientTo: "blue",
+    decimalPlaces: 2, // optional, defaults to 2dp
+    color: (opacity = 1) => `white`,
+    labelColor: (opacity = 1) => `white`,
+    style: {
+        borderRadius: 16
+    }
+    }}
+    style={{
+    marginVertical: 8,
+    borderRadius: 16
+    }}
+/> */}
 
 
 
@@ -155,6 +222,7 @@ export const GroupPage = ({ route, navigation}) => {
         <Text style={{color:'white'}}>Name, Date, Exercise,Weight</Text>
         </View>
 
+        <NavBar navigation={navigation}/>
 
         </View>
     )
